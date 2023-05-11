925700888bc5dba4dde7bdb3b85d49b47f2446ea
AnnisaMutiaraFatika <annisamutiarafatika1@gmail.com>
2023-05-11 16:51:20 +0700

    annisamf

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import pengguna.User;
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
-       //adakah
+        
+        Mhs mhs = new Mhs("username", "password");
+
+        mhs.setNama("Annisa Mutiara Fatika");
+        mhs.setNim("G.111.20.0043");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("USERNAME: " + mhs.getUsername());
+        System.out.println("PASSWORD: " + mhs.getPassword());
     }
 }
